c39a42adaabf4acb9bf334d644ce80aea40157db
vamlin <lsc991123@gmail.com>
2024-08-15 12:17:58 +0900

    feat: init

---
+++ src/main/java/com/publiko/backend/PublikoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.publiko.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublikoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PublikoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/publiko/backend/PublikoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.publiko.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublikoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
